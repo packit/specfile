7f960adb2ed086b545dfba78ffaf9fcc7c0cde1b
Nikola Forró <nforro@redhat.com>
2022-01-11 15:17:00 +0100

    Add methods for working with RPM macros and for parsing spec
    Signed-off-by: Nikola Forró <nforro@redhat.com>

---
+++ specfile/exceptions.py
@@ -0,0 +1,25 @@
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
+
+from typing import List
+
+
+class SpecfileException(Exception):
+    """Something went wrong during our execution."""
+
+    pass
+
+
+class RPMException(SpecfileException):
+    """Exception related to RPM."""
+
+    def __init__(self, stderr: List[bytes]) -> None:
+        super().__init__()
+        self.stderr = stderr
+
+    def __str__(self) -> str:
+        for rline in self.stderr:
+            line = rline.decode().rstrip()
+            if line.startswith("error:"):
+                return line.split("error: ")[1]
+        return "\n" + b"".join(self.stderr).decode()
