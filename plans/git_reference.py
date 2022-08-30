28927ee1b39d672246ca2025fd8207f1659c1555
Nikola Forró <nforro@redhat.com>
2022-08-30 15:27:27 +0200

    Add FMF tests
    Signed-off-by: Nikola Forró <nforro@redhat.com>

---
+++ plans/git_reference.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import subprocess
+from pathlib import Path
+
+import fmf
+
+tree_root = Path.cwd().absolute()
+node = fmf.Tree(tree_root).find("/plans")
+with node as data:
+    data["discover"]["url"] = "https://github.com/packit/specfile.git"
+    data["discover"]["ref"] = (
+        subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
+    )
