446a315ca546b924627e4c89e19a4f654ff8609a
Nikola Forró <nforro@redhat.com>
2023-09-21 15:29:37 +0200

    Stop using deprecated post-release versioning scheme
    Signed-off-by: Nikola Forró <nforro@redhat.com>

--- setup.py
+++ setup.py
@@ -5,4 +5,11 @@
 
 from setuptools import setup
 
-setup(use_scm_version={"version_scheme": "post-release"})
+# we can't use pre-release-based version scheme because it generates versions
+# that are sorted higher than the last stable release by RPM
+# for example:
+#   - pre-release (guess-next-dev):
+#       0.20.1.dev1+g0abcdef.d20230921 > 0.20.1
+#   - post-release (no-guess-dev):
+#       0.20.0.post1.dev1+g0abcdef < 0.20.1
+setup(use_scm_version={"version_scheme": "no-guess-dev"})
